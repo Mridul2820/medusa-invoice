2503d7c9af3b2554c49f00f5c3181081144f68c3
Mridul Panda <immridulpanda2820@gmail.com>
2025-02-06 00:35:40 +0530

    Refactor invoice information layout in invoice-info.ts

--- src/services/templates/invoices/basic/parts/invoice-info.ts
+++ src/services/templates/invoices/basic/parts/invoice-info.ts
@@ -5,24 +5,15 @@ export function generateInvoiceInformation(
   y: number,
   invoice: Invoice
 ): number {
-  doc.fillColor("#444444").fontSize(28).text("Invoice", 50, 50, {
+  doc.fillColor("#444444").fontSize(28).text("Invoice", 40, 50, {
     align: "right",
   });
 
   const invoiceInformationTop = y + 80;
 
-  doc
-    .fontSize(14)
-    .text(invoice.display_number, 150, 130, {
-      align: "right",
-    })
-    .text("Date:", 50, 75, {
-      align: "right",
-    })
-    .text(invoice.created_at.toLocaleDateString(), 150, 85, {
-      align: "right",
-    })
-    .moveDown();
+  doc.fontSize(14).text(invoice.display_number, 40, 70, {
+    align: "right",
+  });
 
   return invoiceInformationTop + 15;
 }
