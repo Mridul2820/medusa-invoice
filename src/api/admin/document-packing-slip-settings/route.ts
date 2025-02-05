ac1159e4902d68602f31c4107a8cd253778053b7
Mridul Panda <immridulpanda2820@gmail.com>
2025-02-05 15:59:30 +0530

    Add Dependabot configuration for daily updates

--- src/api/admin/document-packing-slip-settings/route.ts
+++ src/api/admin/document-packing-slip-settings/route.ts
@@ -10,60 +10,57 @@
  * limitations under the License.
  */
 
-import type { 
-  MedusaRequest, 
-  MedusaResponse,
-} from "@medusajs/medusa"
+import type { MedusaRequest, MedusaResponse } from "@medusajs/medusa";
 import DocumentPackingSlipSettingsService from "../../../services/document-packing-slip-settings";
-import { DocumentPackingSlipSettings } from "../../..//models/document-packing-slip-settings";
+import { DocumentPackingSlipSettings } from "../../../models/document-packing-slip-settings";
 import { PackingSlipTemplateKind } from "../../../services/types/template-kind";
 
-export const GET = async (
-  req: MedusaRequest,
-  res: MedusaResponse
-) => {
-
-  const documentPackingSlipSettingsService: DocumentPackingSlipSettingsService = req.scope.resolve('documentPackingSlipSettingsService');
+export const GET = async (req: MedusaRequest, res: MedusaResponse) => {
+  const documentPackingSlipSettingsService: DocumentPackingSlipSettingsService =
+    req.scope.resolve("documentPackingSlipSettingsService");
 
   try {
-    const lastDocumentPackingSlipSettings: DocumentPackingSlipSettings | undefined = await documentPackingSlipSettingsService.getLastDocumentPackingSlipSettings();
+    const lastDocumentPackingSlipSettings:
+      | DocumentPackingSlipSettings
+      | undefined =
+      await documentPackingSlipSettingsService.getLastDocumentPackingSlipSettings();
     res.status(200).json({
-      settings: lastDocumentPackingSlipSettings
+      settings: lastDocumentPackingSlipSettings,
     });
-    
   } catch (e) {
     res.status(400).json({
-      message: e.message
-    })
+      message: e.message,
+    });
   }
-}
+};
 
-export const POST = async (
-  req: MedusaRequest,
-  res: MedusaResponse
-) => {
-
+export const POST = async (req: MedusaRequest, res: MedusaResponse) => {
   const body: any = req.body as any;
-  const documentPackingSlipSettingsService: DocumentPackingSlipSettingsService = req.scope.resolve('documentPackingSlipSettingsService');
+  const documentPackingSlipSettingsService: DocumentPackingSlipSettingsService =
+    req.scope.resolve("documentPackingSlipSettingsService");
   const formatNumber: string | undefined = body.formatNumber;
   const forcedNumber: string | undefined = body.forcedNumber;
   const template: string | undefined = body.template;
 
   try {
-    const newSettings: DocumentPackingSlipSettings | undefined = await documentPackingSlipSettingsService.updateSettings(formatNumber, forcedNumber, template as PackingSlipTemplateKind);
+    const newSettings: DocumentPackingSlipSettings | undefined =
+      await documentPackingSlipSettingsService.updateSettings(
+        formatNumber,
+        forcedNumber,
+        template as PackingSlipTemplateKind
+      );
     if (newSettings !== undefined) {
       res.status(201).json({
-        settings: newSettings
-      }); 
+        settings: newSettings,
+      });
     } else {
       res.status(400).json({
-        message: 'Cant update settings'
-    })
+        message: "Cant update settings",
+      });
     }
-    
   } catch (e) {
     res.status(400).json({
-        message: e.message
-    })
+      message: e.message,
+    });
   }
-}
+};
--- src/api/admin/document-settings/route.ts
+++ src/api/admin/document-settings/route.ts
@@ -10,29 +10,22 @@
  * limitations under the License.
  */
 
-import type { 
-  MedusaRequest, 
-  MedusaResponse,
-} from "@medusajs/medusa"
+import type { MedusaRequest, MedusaResponse } from "@medusajs/medusa";
 import InvoiceService from "../../../services/invoice";
-import { DocumentSettings } from "../../..//models/document-settings";
+import { DocumentSettings } from "../../../models/document-settings";
 
-export const GET = async (
-  req: MedusaRequest,
-  res: MedusaResponse
-) => {
-
-  const invoiceService: InvoiceService = req.scope.resolve('invoiceService');
+export const GET = async (req: MedusaRequest, res: MedusaResponse) => {
+  const invoiceService: InvoiceService = req.scope.resolve("invoiceService");
 
   try {
-    const documentSettings: DocumentSettings | undefined = await invoiceService.getLastDocumentSettings();
+    const documentSettings: DocumentSettings | undefined =
+      await invoiceService.getLastDocumentSettings();
     res.status(200).json({
-      settings: documentSettings
+      settings: documentSettings,
     });
-    
   } catch (e) {
     res.status(400).json({
-      message: e.message
-    })
+      message: e.message,
+    });
   }
-}
+};
